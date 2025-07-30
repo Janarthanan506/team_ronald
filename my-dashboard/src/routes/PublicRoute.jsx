05f5646a9beb44718d7570e151e09a83cbc7ed04
Rogers <janajanarthanan@imaggar.in>
2025-07-30 12:15:25 +0530

    Delete my-dashboard directory

--- my-dashboard/src/routes/PublicRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-export default function PublicRoute({ children }) {
-  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-
-  return isLoggedIn ? <Navigate to="/dashboard" /> : children;
-}
